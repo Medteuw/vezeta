5ca4cc29930d5af16a58d42784cb0a5fcfa8db1e
medteuw <medteuw@gmail.com>
2020-12-24 22:09:09 +0000

    add a slug field & the default profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.utils.text import slugify
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     name = models.CharField("Se tour",max_length=50)
     who_i = models.TextField("Yay kane")
-    price = models.IntegerField()
-    image = models.ImageField('Se photo',upload_to='profils')
+    price = models.IntegerField(blank=True,null=True)
+    image = models.ImageField('Se photo',upload_to='profils',blank=True,null=True)
+    slug = models.SlugField("slug", blank=True,null=True)
+
+    def save(self, *args,**kwargs):
+        if not self.slug:
+            self.slug = slugify(self.user.username)
+        super(Profile,self).save(*args,**kwargs)
+
 
 
     def __str__(self):
-        return self.name
+        return '%s' %(self.user.username)
+
+
+def create_profile(sender,**kwargs):
+    if kwargs['created']:
+        Profile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
